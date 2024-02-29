d56ba5f9ce4178a9cdc5ff9f5ca1b404b4d48b61
MrChen-H <3175073980@qq.com>
2024-02-29 22:28:39 +0800

    添加 spdlog 依赖

--- Cloud_File_Server/src/GetPathInfo.cpp
+++ Cloud_File_Server/src/GetPathInfo.cpp
@@ -1,17 +1,4 @@
-#include <fcgiapp.h>
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <cstring>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <wait.h>
-#include "json.hpp"
-#include "fcgi_stdio.h"
-#include "fcgio.h"
-#include "fcgiapp.h"
-
+#include "GetPathInfo.h"
 using json = nlohmann::json;
 int main()
 {
---
+++ Cloud_File_Server/src/GetPathInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <fcgiapp.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstring>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <wait.h>
+#include "json.hpp"
+#include "fcgi_stdio.h"
+#include "fcgio.h"
+#include "fcgiapp.h"
+
