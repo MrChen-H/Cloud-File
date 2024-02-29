fb40c51f85e649e455924334594e897f85e91050
MrChen-H <3175073980@qq.com>
2024-03-01 00:54:39 +0800

    完成所有文件信息发送接口

--- Cloud_File_Server/src/GetAllFileInfo.cpp
+++ Cloud_File_Server/src/GetAllFileInfo.cpp
@@ -1,5 +1,5 @@
-#include "GetPathInfo.h"
-using json = nlohmann::json;
+#include "GetAllFileInfo.h"
+
 int main()
 {
     FCGX_Stream *input, *output, *error;
@@ -21,13 +21,21 @@ int main()
         }
         auto lenght = atoi(pLenstr);
         std::string information;
-        char reply[128];
-        sprintf(reply,"{\"info\":\"%s\"}",information.c_str());
+
+        std::string res_info = GetPathInfo(lenght,input,information);
+        char reply[1024];
+        if(res_info.empty())
+        {
+            FCGX_FPrintF(output, "Content-type: application/json\r\n"
+                             "Charset: utf-8\r\n"
+                             "\r\n"
+                             "{\"file_name\":\"null\"}");   
+        }
         // 数据回写
         FCGX_FPrintF(output, "Content-type: application/json\r\n"
                              "Charset: utf-8\r\n"
                              "\r\n"
-                             "{""testServer"":""success""}");   
+                             "%s",res_info.c_str());   
     }
     return 0;
 }
